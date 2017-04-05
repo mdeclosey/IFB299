62b403c8eeec0f421a8de0806b7c36e4cdeb19f1
JasonTrapp <jason_t_28@protonmail.ch>
2017-04-05 10:15:36 +1000

    Entered wrong file

--- development_files/tenant.sql
+++ development_files/tenant.sql
@@ -1,15 +1,17 @@
-CREATE TABLE Staff(
-    staffID INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE TENANT(
+    tenantID INT NOT NULL AUTO_INCREMENT,
     fName varchar(40) NOT NULL,
     lname varchar(40) NOT NULL,
     email varchar(60) NOT NULL,
     phone INT NOT NULL,
-    propertiesOwned NOT NULL,
-
+    propertyRented INT NOT NULL,
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL,
+    dayOfPayments INT NOT NULL,
+    paymentFrequency varchar(40) NOT NULL,
+    isStaff BOOLEAN NOT NULL,
 );
 
-INSERT INTO Staff (fname, lname, email, phone) VALUES (Bob, Jones, bobby@example.com, 55555555);
-INSERT INTO Staff (fname, lname, email, phone) VALUES (David, Allen, DavidAllen@example.com, 55555556);
-INSERT INTO Staff (fname, lname, email, phone) VALUES (Greg, Davies, Greg@example.com, 55555557);
-INSERT INTO Staff (fname, lname, email, phone) VALUES (Bob, Jones, bobby@example.com, 55555558);
-INSERT INTO Staff (fname, lname, email, phone) VALUES (Bob, Jones, bobby@example.com, 55555559);
+INSERT INTO tenant (fname, lname, email, phone, propertyRented, startDate, endDate, dayOfPayments,
+    paymentFrequency, isStaff) VALUES ("David", "Jones", davidJones@example.com, 55444444, 14, 
+    01/02/2017, 01/01/2018, 1, "Fortnightly", False);
