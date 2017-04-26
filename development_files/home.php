b483226ece1a6bda889d49ce396af9ac1f763ed4
jakeb1996 <jacob.bradford@connect.qut.edu.au>
2017-04-26 11:52:51 +1000

    Implemented basic search function. Search bar and button layout broke, why??

--- development_files/home.php
+++ development_files/home.php
@@ -4,17 +4,34 @@
 		<div class="panel panel-primary">
 			<div class="panel-heading">
 				<div class="input-group">
-				  <input type="text" class="form-control" placeholder="Search for...">
-				  <span class="input-group-btn">
-					<button class="btn btn-secondary btn-success" type="button">Go!</button>
-				  </span>
+					<form action="home.php" method="get" id="frmSearch">
+						<input type="text" name="q" class="form-control" placeholder="Search for..." value="<?php echo isset($_GET['q']) ? $_GET['q'] : '' ?>">
+						<span class="input-group-btn">
+							<button class="btn btn-secondary btn-success" type="button" id="btnSearch">Go!</button>
+						</span>
+					</form>
 				</div>
 			</div>
 			<div class="panel-body">
-				<div class="panel-body">Panel Content</div>
-				<div class="panel-body">Panel Content</div>
-				<div class="panel-body">Panel Content</div>
+				<?php
+			
+					if (isset($_GET['q'])) {
+						$searchQuery = mysqli_query($db, "SELECT * FROM properties WHERE street LIKE '%{$_GET['q']}%' OR suburb  LIKE '%{$_GET['q']}%'") or die(mysqli_error($db));
+						while ($result = mysqli_fetch_assoc($searchQuery)) {
+							echo "<div class=\"panel-body\"><a href=\"properties.php?id={$result['propertyID']}\">{$result['street']}, {$result['suburb']}, {$result['postcode']}</a></div>";
+						}
+					} else {
+						echo 'Nothing to show.';
+					}				
+				?>
 			</div>
 		</div>
 	
+		<script>
+			$(document).ready(function() {
+				$('#btnSearch').on('click', function() {
+					$('#frmSearch').submit();
+				});
+			});
+		</script>
 <?php include('pageFooter.php'); ?>
