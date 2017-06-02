651a8d334b517323f34b533b2d70740283ec578e
JasonTrapp <jason_t_28@protonmail.ch>
2017-06-02 17:40:01 +1000

    Add files via upload

---
+++ Artifacts2/inspection.php
@@ -0,0 +1,285 @@
+<?php include('helper.php'); ?>
+
+<?php
+/***** New/Edit contract *****/
+if (isset($_GET['save'])) {
+	if (isset($_POST['id']) && $_POST['id'] > 0) {
+		// should perform server side form validation but meh,
+		// if jquery caught the save click then it must have been validated already (but consider csrf)
+		$update = mysqli_query($db, "UPDATE property_view_tenants SET property_inspection_id='{$_POST['propertyID']}', tenant_id='{$_POST['tenantID']}' WHERE id='{$_POST['id']}'");
+		
+		// if the update was successful
+		if ($update) {
+			header('location: inspection.php'); // redirect to prevent resubmit
+		} else {
+			echo mysqli_error($db); exit;
+		}
+	} else {
+		// new contract
+		$update = mysqli_query($db, "INSERT INTO property_view_tenants (property_inspection_id, tenant_id) VALUES('{$_POST['propertyID']}', '{$_POST['tenantID']}')");
+		
+		// if the update was successful
+		if ($update) {
+			header('location: inspection.php'); // redirect to prevent resubmit
+		} else {
+			echo mysqli_error($db); exit;
+		}
+	}
+}
+
+/***** Delete contract *****/
+if (isset($_GET['delete'])) {
+	if (isset($_GET['id']) && $_GET['id'] > 0) {
+		$delete = mysqli_query($db, "DELETE FROM property_view_tenants WHERE id={$_GET['id']}");
+		
+		if ($delete) {
+			header('location: inspection.php'); // redirect to prevent resubmit
+		} else {
+			echo mysqli_error($db); exit;
+		}
+	} else {
+		header('location: inspection.php');
+	}
+}
+
+/***** Quick register for inspection ******/
+if (isset($_GET['register'])) {
+	$update = mysqli_query($db, "INSERT INTO property_view_tenants (property_inspection_id, tenant_id) VALUES('{$_GET['register']}', '{$_SESSION['user_id']}')");
+		
+	// if the update was successful
+	if ($update) {
+		header('location: properties.php?view&id='.$_GET['property']); // redirect to prevent resubmit
+	} else {
+		echo mysqli_error($db); exit;
+	}
+}
+?>
+
+<?php include('pageHeader.php'); ?>
+
+<?php
+
+// List contracts
+if (count($_GET) == 0) { // this line checks we are only listing contracts, nothing else
+	if ($_SESSION['user_type'] == 'david') { // list all property contracts because david is logged in
+		$inspectList = mysqli_query($db, "
+			SELECT 
+			property_view_tenants.id, 
+			property_view_tenants.property_inspection_id, 
+			properties.street, 
+			properties.suburb, 
+			properties.postcode, 
+			properties.staffID,
+			property_view_tenants.tenant_id, 
+			tenants.fName,
+			tenants.lname,
+			PropertyViews.start_datetime,
+			PropertyViews.end_dateTime,
+			staff.fName as staff_fName,
+			staff.lname as staff_lname
+			FROM property_view_tenants
+			LEFT JOIN tenants ON tenants.tenantID = property_view_tenants.tenant_id
+			LEFT JOIN properties ON properties.propertyID = property_view_tenants.property_inspection_id
+			LEFT JOIN staff ON properties.staffID = staff.staffID
+			LEFT JOIN PropertyViews ON PropertyViews.propertyID = property_view_tenants.property_inspection_id
+			ORDER BY PropertyViews.start_datetime ASC
+		") or die(mysqli_error($db));
+	} else { // only list property contracts for the current users' properties
+		$inspectList = mysqli_query($db, "
+			SELECT 
+			property_view_tenants.id, 
+			property_view_tenants.property_inspection_id, 
+			properties.street, 
+			properties.suburb, 
+			properties.postcode, 
+			properties.staffID AS myStaff,
+			property_view_tenants.tenant_id, 
+			tenants.fName,
+			tenants.lname,
+			PropertyViews.start_datetime,
+			PropertyViews.end_dateTime,
+			staff.fName as staff_fName,
+			staff.lname as staff_lname
+			FROM property_view_tenants
+			LEFT JOIN tenants ON tenants.tenantID=property_view_tenants.tenant_id
+			LEFT JOIN properties ON properties.propertyID=property_view_tenants.property_inspection_id
+			LEFT JOIN staff ON properties.staffID = staff.staffID
+			LEFT JOIN PropertyViews ON PropertyViews.propertyID=property_view_tenants.property_inspection_id
+			WHERE properties.staffID = {$_SESSION['user_id']}
+			ORDER BY PropertyViews.start_datetime ASC
+		") or die(mysqli_error($db));
+	}
+	?>
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			Viewing List 
+			<span style="float:right; margin-top: -5.5px">
+				<a href='inspection.php?new' class='btn btn-success btn-sm'>
+				  <span class='glyphicon glyphicon-plus'></span> Register Tenant for Inspection
+				</a>
+			</span>
+		</div>
+		<div class="panel-body">
+			<table class="table">
+			  <thead>
+				<tr>
+				  <th>#</th>
+				  <th>Property</th>
+				  <th>Tenant</th>
+				  <th>Staff</th>
+				  <th>Date Time</th>
+				  <th>Actions</th>
+				</tr>
+			  </thead>
+				<tbody>
+					<?php
+					
+					// loop over each contract and display it
+					while ($inspection = mysqli_fetch_assoc($inspectList)) {
+						$start = date('l jS F Y g:ia ', strtotime($inspection['start_datetime']));
+						$start_end   = $start . ' - ' . date('g:ia', strtotime($inspection['end_dateTime']));
+						echo "
+							<tr>
+							  <th scope='row'>{$inspection['id']}</th>
+							  <td>{$inspection['street']}, {$inspection['suburb']}, {$inspection['postcode']}</td>
+							  <td>{$inspection['fName']} {$inspection['lname']}</td>
+							  <td>{$inspection['staff_fName']} {$inspection['staff_lname']}</td>
+							  <td>{$start_end}</td>
+							  <td>
+								<a href='inspection.php?edit&id={$inspection['id']}' class='btn btn-success'>
+								  <span class='glyphicon glyphicon-edit'></span> Edit
+								</a>
+								<a href='inspection.php?delete&id={$inspection['id']}' class='btn btn-danger'>
+								  <span class='glyphicon glyphicon-trash'></span> Delete
+								</a>
+							  </td>
+							</tr>
+						";
+					}	
+					?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<?php
+}
+
+
+// Create/Edit contract form
+if (isset($_GET['edit']) && isset($_GET['id']) && $_GET['id'] > 0 ||
+	isset($_GET['new'])) { 
+	
+	if (isset($_GET['edit'])) {
+		// edit mode
+		echo "<script>console.log('test');</script>";
+		$contract = mysqli_fetch_assoc(mysqli_query($db, "SELECT * FROM property_view_tenants WHERE property_inspection_id={$_GET['id']}"));
+		echo "<script>console.log('test');</script>";
+		$action = 'Edit Inspection #';
+	} else {
+		// create mode
+		// fill an empty array representing the new tenant. for quick hacks of the existing edit form
+		$contract = [
+			'property_inspection_id' => '',
+			'tenant_id' => '',
+			
+		];
+		
+		$action = 'Inspection Registration';
+	}
+	
+	$tenants = mysqli_query($db, "SELECT * FROM tenants");
+	if ($_SESSION['user_type'] == 'david') {
+		$properties = mysqli_query($db, "SELECT *, propertyviews.id as propertyviews_id FROM properties RIGHT JOIN propertyviews ON properties.propertyID=propertyviews.propertyID ORDER BY PropertyViews.start_datetime ASC");
+	} else {
+		$properties = mysqli_query($db, "SELECT *, propertyviews.id as propertyviews_id FROM properties RIGHT JOIN propertyviews ON properties.propertyID=propertyviews.propertyID WHERE properties.staffID = '". $_SESSION['user_id'] ."' ORDER BY PropertyViews.start_datetime ASC");
+	}
+?>
+	<form action="inspection.php?save" method="post" id="frmEditContract">
+		<?php if (isset($_GET['edit'])) { ?> <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">  <?php }; ?>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<?php echo "{$action} {$contract['property_inspection_id']}"; ?>
+			</div>
+			<div class="panel-body" style="padding: 2em">
+				<div class="form-group row">
+				  <label for="example-text-input" class="col-2 col-form-label">Property</label>
+				  <div class="col-10">
+					<select name="propertyID" id="propertyID" class="form-control">
+						<option disabled>Select property inspection</option>
+						<?php
+						while ($property = mysqli_fetch_assoc($properties)) {
+							if ($property['propertyviews_id'] == $contract['property_inspection_id']) {
+								echo "<option selected value='{$property["propertyviews_id"]}'>";
+							} else {
+								echo "<option value='{$property["propertyviews_id"]}'>";
+							}
+							
+							$start = date('l jS F Y g:ia ', strtotime($property['start_datetime']));
+							$start_end   = $start . ' - ' . date('g:ia', strtotime($property['end_dateTime']));
+							
+							echo "{$property["street"]} {$property["suburb"]} {$property["postcode"]} @  {$start_end}</option>";
+						}
+						?>
+					</select>
+				  </div>
+				</div>
+				<div class="form-group row">
+				  <label for="example-search-input" class="col-2 col-form-label">Tenant</label>
+				  <div class="col-10">
+					<select name="tenantID" id="tenantID" class="form-control">
+						<option disabled>Select tenant</option>
+						<?php
+						while ($tenant = mysqli_fetch_assoc($tenants)) {
+							if ($tenant['tenantID'] == $contract['tenant_id']) {
+								echo "<option selected value='{$tenant["tenantID"]}'>";
+							} else {
+								echo "<option value='{$tenant["tenantID"]}'>";
+							}
+							echo "{$tenant["fName"]} {$tenant["lname"]}</option>";
+						}
+						?>
+					</select>
+				  </div>
+				</div>
+				<div class="form-group row">
+				  <div class="col-10">
+					<button type="button" id="save" class='btn btn-success'>
+					  <span class='glyphicon glyphicon-ok-circle'></span> Save
+					</button>
+					<button type="button" id="cancel" class='btn btn-secondary'>
+					  <span class='glyphicon glyphicon-remove-circle'></span> Cancel
+					</button>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</form>
+	
+	<script>
+		$(document).ready(function() {
+			// Cancel button pressed
+			$('#cancel').click(function() {
+				window.location.href = "inspection.php";
+			});
+			
+			// Save button pressed
+			$('#save').click(function() {
+				var property = $("#propertyID");
+				var tenant = $("#tenantID");
+				
+				// validate form
+				if (property.val() <= 0 ||
+					tenant.val() <= 0) 
+				{
+					alert('Ensure you have filled out the entire form.');
+				} else {
+					$("#frmEditContract").submit();
+				}
+			});
+		});
+	</script>
+	<?php
+}
+?>
+
+<?php include('pageFooter.php'); ?>
---
+++ Artifacts2/propertyViews.php
@@ -0,0 +1,240 @@
+<?php include('helper.php'); ?>
+
+<?php
+/***** New/Edit viewing *****/
+if (isset($_GET['save'])) {
+
+    if (isset($_POST['id']) && $_POST['id'] > 0) {
+        // if jquery caught the save click then it must have been validated already (but consider csrf)
+	
+        print_r($_POST);
+	
+        $update = mysqli_query($db, "UPDATE PropertyViews SET propertyID={$_POST['propertyID']}, start_datetime='{$_POST['start_datetime']}', end_dateTime='{$_POST['end_datetime']}' WHERE id='{$_POST['id']}'");
+
+        if ($update) {
+            header('location: propertyViews.php'); // redirect to prevent resubmit
+        } else {
+            echo mysqli_error($db); exit;
+        }
+    } else {
+        // new viewing
+	$update = mysqli_query($db, "INSERT INTO PropertyViews (propertyID, start_datetime, end_dateTime, staffID) VALUES('{$_POST['propertyID']}', '{$_POST['start_datetime']}', '{$_POST['end_datetime']}','{$_POST['propertyID']}')");
+
+        if ($update) {
+            header('location: propertyViews.php'); // redirect to prevent resubmit
+        } else {
+            echo mysqli_error($db); exit;
+        }
+    }
+}
+
+/***** Delete viewing *****/
+if (isset($_GET['delete'])) {
+    if (isset($_GET['id']) && $_GET['id'] > 0) {
+        $delete = mysqli_query($db, "DELETE FROM PropertyViews WHERE id={$_GET['id']}");
+
+        if ($delete) {
+            header('location: propertyViews.php'); // redirect to prevent resubmit
+        } else {
+            echo mysqli_error($db); exit;
+        }
+    } else {
+        header('location: propertyViews.php');
+    }
+}
+?>
+
+<?php include('pageHeader.php'); ?>
+
+
+<?php
+// List properties
+if (count($_GET) == 0) {
+	$where = "";
+	
+	if ($_SESSION['user_type'] != 'david') {
+		$where = "WHERE properties.staffID=".$_SESSION['user_id'];
+	}
+	
+    $viewsList = mysqli_query($db, "
+		SELECT
+		PropertyViews.id as PropertyViews_id,
+		properties.street, 
+		properties.suburb, 
+		properties.postcode,
+		staff.fname,
+		staff.lname, 
+		PropertyViews.id,
+		PropertyViews.propertyID,
+		PropertyViews.start_datetime,
+		PropertyViews.end_datetime,
+		PropertyViews.staffID
+		FROM PropertyViews
+		LEFT JOIN properties ON PropertyViews.propertyID=properties.propertyID
+		LEFT JOIN staff ON PropertyViews.staffID=staff.staffID
+		".$where."
+		ORDER BY PropertyViews.start_datetime ASC
+	") or die(mysqli_error($db));
+    ?>
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            Property Viewings
+            <span style="float:right; margin-top: -5.5px">
+				<a href='propertyViews.php?new' class='btn btn-success btn-sm'>
+				  <span class='glyphicon glyphicon-plus'></span> New Viewing Schedule
+				</a>
+			</span>
+        </div>
+        <div class="panel-body">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Property</th>
+                    <th>Staff</th>
+                    <th>Date Time</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                while ($view = mysqli_fetch_assoc($viewsList)) {
+					$start = date('l jS F Y g:ia ', strtotime($view['start_datetime']));
+					$start_end   = $start . ' - ' . date('g:ia', strtotime($view['end_datetime']));
+                    echo "
+							<tr>
+							  <th scope='row'>{$view['PropertyViews_id']}</th>
+							  <td>{$view['street']}, {$view['suburb']}, {$view['postcode']}</td>
+							  <td>{$view['fname']} {$view['lname']}</td>
+							  <td>{$start_end}</td>
+							  <td>
+								<a href='propertyViews.php?edit&id={$view['id']}' class='btn btn-success'>
+								  <span class='glyphicon glyphicon-edit'></span> Edit
+								</a>
+								<a href='propertyViews.php?delete&id={$view['PropertyViews_id']}' class='btn btn-danger'>
+								  <span class='glyphicon glyphicon-trash'></span> Delete
+								</a>
+							  </td>
+							</tr>
+						";
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <?php
+}
+
+
+// Create/Edit contract
+if (isset($_GET['edit']) && isset($_GET['id']) && $_GET['id'] > 0 ||
+	isset($_GET['new'])) { 
+	
+	if (isset($_GET['edit'])) {
+		// edit mode
+		$views = mysqli_fetch_assoc(mysqli_query($db, "SELECT * FROM PropertyViews WHERE id={$_GET['id']}"));
+		$action = 'Edit Viewing Time #';
+	} else {
+		// create mode
+		// fill an empty array representing the new tenant. for quick hacks of the existing edit form
+		$views = [
+			'id' => '',
+			'start_datetime' => '',
+			'end_datetime' => '',
+		];
+		
+		$action = 'New Viewing Time';
+	}
+	
+
+	if ($_SESSION['user_type'] == 'david') {
+		$properties = mysqli_query($db, "SELECT * FROM properties LEFT JOIN staff ON staff.staffID=properties.staffID");
+	} else {
+		$properties = mysqli_query($db, "SELECT * FROM properties LEFT JOIN staff ON staff.staffID=properties.staffID WHERE properties.staffID='" . $_SESSION['user_id'] . "'");
+	}
+	$staff = mysqli_query($db, "SELECT * FROM staff");
+?>
+	<form action="propertyViews.php?save" method="post" id="frmEditView">
+		<?php if (isset($_GET['edit'])) { ?> <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">  <?php }; ?>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<?php echo "{$action} {$views['id']}"; ?>
+			</div>
+			<div class="panel-body" style="padding: 2em">
+				<div class="form-group row">
+				  <label for="example-text-input" class="col-2 col-form-label">Property</label>
+				  <div class="col-10">
+					<select name="propertyID" id="propertyID" class="form-control">
+						<option disabled>Select property</option>
+						<?php
+						while ($property = mysqli_fetch_assoc($properties)) {
+							if ($property['propertyID'] == $views['propertyID']) {
+								echo "<option selected value='{$property["propertyID"]}'>";
+							} else {
+								echo "<option value='{$property["propertyID"]}'>";
+							}
+							echo "{$property["street"]} {$property["suburb"]} {$property["postcode"]}</option>";
+							
+							
+						}
+						?>
+					</select>
+				  </div>
+				</div>
+				<div class="form-group row">
+				  <label for="example-email-input" class="col-2 col-form-label">Start Date</label>
+				  <div class="col-10">
+					<input class="form-control" type="datetime-local" value="<?php echo date("Y-m-d\TH:i:00", strtotime($views['start_datetime'])); ?>" id="start_datetime" name="start_datetime">
+				  </div>
+				</div>
+				<div class="form-group row">
+				  <label for="example-url-input" class="col-2 col-form-label">End Date</label>
+				  <div class="col-10">
+					<input class="form-control" type="datetime-local" value="<?php echo date("Y-m-d\TH:i:00", strtotime($views['end_dateTime'])); ?>" id="end_datetime" name="end_datetime">
+				  </div>
+				</div>
+				<div class="form-group row">
+				  <div class="col-10">
+					<button type="button" id="save" class='btn btn-success'>
+					  <span class='glyphicon glyphicon-ok-circle'></span> Save
+					</button>
+					<button type="button" id="cancel" class='btn btn-secondary'>
+					  <span class='glyphicon glyphicon-remove-circle'></span> Cancel
+					</button>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</form>
+
+
+	<script>
+		$(document).ready(function() {
+			// Cancel button pressed
+			$('#cancel').click(function() {
+				window.location.href = "propertyViews.php";
+			});
+			
+			// Save button pressed
+			$('#save').click(function() {
+				var property = $("#propertyID");
+				var startDate = $("#start_datetime");
+				var endDate = $("#end_datetime");
+				
+				// validate form
+				if (
+					startDate.val() == '' ||
+					endDate.val() == '') 
+				{
+					alert('Ensure you have filled out the entire form.');
+				} else {
+					$("#frmEditView").submit();
+				}
+			});
+		});
+	</script>
+	<?php
+}
+?>
+<?php include('pageFooter.php'); ?>
